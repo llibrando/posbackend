88a067ae1a03866cecb96e0c6e6a8abb874869c4
llibrando <142190405+llibrando@users.noreply.github.com>
2024-04-07 00:52:28 +0800

    Add files via upload

---
+++ posbackend/README.md
@@ -0,0 +1,13 @@
+# posbackend
+>conda create --name pos python=3.9
+>
+>conda activate pos
+>
+>conda install uvicorn, bcrypt
+>
+>conda install -c conda-forge fastapi
+>
+>conda install mysql-connector-python
+>
+>git clone https://github.com/llibrando/posbackend
+
---
+++ posbackend/main.py
@@ -0,0 +1,19 @@
+# main.py
+from fastapi import FastAPI
+import mysql.connector
+from model.cashier import cashierRouter
+from model.menu import menuRouter
+from model.payment import paymentRouter
+from model.orders import ordersRouter
+from model.transaction import transactionRouter
+
+
+app = FastAPI()
+
+# Include CRUD routes from modules
+app.include_router(cashierRouter, prefix="/api")
+app.include_router(menuRouter, prefix="/api")
+app.include_router(ordersRouter, prefix="/api")
+app.include_router(paymentRouter, prefix="/api")
+app.include_router(transactionRouter, prefix="/api")
+
