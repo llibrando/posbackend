0f25d3f051dc84250dea9496706e216e55e9cc9e
llibrando <142190405+llibrando@users.noreply.github.com>
2024-04-06 21:02:17 +0800

    Add files via upload

---
+++ Canteen_POS/model/cashier.py
@@ -0,0 +1,122 @@
+# model/cashier.py
+from fastapi import APIRouter, HTTPException, Depends
+from pydantic import BaseModel
+from typing import List
+from .db import get_db 
+from typing import Optional
+from pydantic import BaseModel
+import mysql.connector
+import bcrypt
+class CashierUpdate(BaseModel):
+  
+    username: str
+    password: str
+class CashierCreate(BaseModel):
+    username: str
+    password: str
+
+cashierRouter = APIRouter(tags=["Cashier"]) 
+
+# CRUD operation
+
+@cashierRouter.get("/cashier/", response_model=list)
+async def read_cashier(
+    db=Depends(get_db)
+):
+    query = "SELECT CashierID, username, password FROM cashier"
+    db[0].execute(query)
+    cashier = [{"CashierID": user[0], "username": user[1], "password": user[2]} for user in db[0].fetchall()]
+    return cashier
+
+
+@cashierRouter.get("/cashier/{CashierID}", response_model=dict)
+async def read_cashier(
+    CashierID: int, 
+    db=Depends(get_db)
+):
+    query = "SELECT CashierID, username FROM cashier WHERE CashierID = %s"
+    db[0].execute(query, (CashierID,))
+    cashier = db[0].fetchone()
+    if cashier:                             
+        return {
+            "CashierID": cashier[0],
+            "username": cashier[1],
+        }
+    raise HTTPException(status_code=404, detail="Cashier not found")
+
+
+
+
+# --------------------POST---------------------------------------------------------------
+@cashierRouter.post("/cashier/", response_model=CashierCreate)
+async def create_cashier(cashier: CashierCreate, db=Depends(get_db)):
+    try:
+        # Construct the SQL query to insert a new cashier
+        query = "INSERT INTO cashier (username, password) VALUES (%s, %s)"
+        # Execute the query with the provided data
+        db[0].execute(query, (cashier.username, cashier.password))
+        # Commit the transaction
+        db[1].commit()
+        # Return the created cashier
+        return cashier
+    except Exception as e:
+        # If an error occurs, raise an HTTPException with a 500 status code
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+    finally:
+        # Close the cursor and database connection
+        db[0].close()
+
+
+
+# -------------------------------PUT/UPDATE---------------------------------------------
+@cashierRouter.put("/cashier/{cashier_id}", response_model=dict)
+async def update_cashier(cashier_id: int, cashier_update: CashierUpdate, db=Depends(get_db)):
+    try:
+        query_check_user = "SELECT `cashierID` FROM `cashier` WHERE `cashierID` = %s"
+        db[0].execute(query_check_user, (cashier_id,))
+        existing_user = db[0].fetchone()
+
+        if not existing_user:
+            raise HTTPException(status_code=404, detail="Cashier not found")
+
+        query_update_user = "UPDATE `cashier` SET `username` = %s, `password` = %s WHERE `cashierID` = %s"
+        db[0].execute(query_update_user, (cashier_update.username, cashier_update.password, cashier_id))
+        db[1].commit()
+
+        return {"message": "Cashier updated successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+    finally:
+        db[0].close()
+
+
+
+# -------------------DELETE------------------------------------------------------
+@cashierRouter.delete("/cashier/{cashier_id}")
+async def delete_cashier(cashier_id: int, 
+                        db=Depends(get_db)):
+    try:
+        # Check if the cashier exists
+        query_check_cashier = "SELECT cashierID FROM cashier WHERE cashierID = %s"
+        db[0].execute(query_check_cashier, (cashier_id,))
+        existing_cashier = db[0].fetchone()
+
+        # If the cashier does not exist, raise HTTPException with 404 status code
+        if not existing_cashier:
+            raise HTTPException(status_code=404, detail="Cashier not found")
+
+        # Delete the cashier from the database
+        query_delete_cashier = "DELETE FROM cashier WHERE cashierID = %s"
+        db[0].execute(query_delete_cashier, (cashier_id,))
+        db[1].commit()
+
+        # Return success message
+        return {"message": "Cashier deleted successfully"}
+    except Exception as e:
+        # If an error occurs, raise HTTPException with 500 status code
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+    finally:
+        # Close the cursor and database connection
+        db[0].close()
+
+
---
+++ Canteen_POS/model/menu.py
@@ -0,0 +1,110 @@
+# model/menu.py
+from fastapi import Depends, HTTPException, APIRouter, Form
+from .db import get_db
+import bcrypt
+
+menuRouter = APIRouter(tags=["Menu"])
+
+# CRUD operations
+
+@menuRouter.get("/menu/", response_model=list)
+async def read_menu(
+    db=Depends(get_db)
+):
+    query = "SELECT ItemID, menuItemName FROM menu"
+    db[0].execute(query)
+    menu = [{"ItemID": user[0], "menuItemName": user[1]} for user in db[0].fetchall()]
+    return menu
+
+
+@menuRouter.get("/menu/{ItemID}", response_model=dict)
+async def read_menu(
+    ItemID: int, 
+    db=Depends(get_db)
+):
+    query = "SELECT ItemID, menuItemName FROM menu WHERE ItemID = %s"
+    db[0].execute(query, (ItemID,))
+    menu = db[0].fetchone()
+    if menu:                             
+        return {
+            "ItemID": menu[0],
+            "menuItemName": menu[1],
+        }
+    raise HTTPException(status_code=404, detail="Item not found")
+
+
+# -----------------------------POST/CREATE----------------------------------
+
+@menuRouter.post("/menu/", response_model=dict)
+async def create_item(
+    item_id: int = Form(...), 
+    menuItemCategory: str = Form(...), 
+    menuItemName: str = Form(...),
+    menuItemPrice: int = Form(...),  
+    db=Depends(get_db)
+):
+
+    query = "INSERT INTO menu (ItemID, menuItemCategory, menuItemName, menuItemPrice) VALUES (%s, %s, %s, %s)"
+    db[0].execute(query, (item_id, menuItemCategory, menuItemName, menuItemPrice))
+
+    # Retrieve the last inserted ID using LAST_INSERT_ID()
+    db[0].execute("SELECT LAST_INSERT_ID()")
+    new_user_id = db[0].fetchone()[0]
+    db[1].commit()
+
+    return {"item_id": new_user_id, 
+            "menuItemCategory": menuItemCategory,
+            "menuItemName": menuItemName,
+            "menuItemPrice": menuItemPrice,     
+            }
+
+
+
+
+# ----------------PUT/UPDATE------------------------------------------------
+@menuRouter.put("/menu/{menu_id}", response_model=dict)
+async def update_order(
+    menu_id: int,
+    menuItemCategory: str,
+    menuItemName: str,
+    menuItemPrice: int,
+    db=Depends(get_db)
+):
+
+    # Update menu information in the database 
+    query = "UPDATE menu SET menuItemCategory = %s, menuItemName = %s, menuItemPrice =%s  WHERE ItemID = %s"
+    db[0].execute(query, (menuItemCategory, menuItemName, menuItemPrice, menu_id))
+
+    # Check if the update was successful
+    if db[0].rowcount > 0:
+        db[1].commit()
+        return {"message": "Item updated successfully"}
+    
+    # If no rows were affected, cashier not found
+    raise HTTPException(status_code=404, detail="Item not found")
+
+
+
+
+
+# --------------------DELETE--------------------------------
+@menuRouter.delete("/menu/{menu_id}", response_model=dict)
+async def delete_menu(
+    menu_id: int,
+    db=Depends(get_db)
+):
+    # try:
+        # Check if the item exists
+        query_check_menu = "SELECT ItemID FROM menu WHERE ItemID = %s"
+        db[0].execute(query_check_menu, (menu_id,))
+        existing_item = db[0].fetchone()
+
+        if not existing_item:
+            raise HTTPException(status_code=404, detail="Item not found")
+
+        # Delete the item
+        query_delete_item = "DELETE FROM menu WHERE ItemID = %s"
+        db[0].execute(query_delete_item, (menu_id,))
+        db[1].commit()
+
+        return {"message": "Item deleted successfully"}
